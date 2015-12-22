6666c0163b4a1de5d6b0886089b998d73fd7612e
bishop <coredumpproject@gmail.com>
2015-12-22 16:27:52 -0500

    initial commit after project layout created.

---
+++ desktop/src/com/coredumpproject/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.coredumpproject.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coredumpproject.game.LanceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LanceGame(), config);
+	}
+}
